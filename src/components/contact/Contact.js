a9d61180e17bf78e7bab70f7fc1cd0287353972e
Harel <harelperi2@gmail.com>
2020-10-26 17:17:07 -0400

    contact update

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,35 +1,59 @@
-import React from "react";
+import React, {useState} from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {faEnvelope, faPhoneSquareAlt} from '@fortawesome/free-solid-svg-icons'
+import {faEnvelope, faPhoneSquareAlt} from '@fortawesome/free-solid-svg-icons';
+import axios from 'axios';
 
 const Contact = (props) =>{
+    const [isSubmitting, setIsSubmitting] = useState(false);
+    const handleSubmit = async (evt) => {
+    evt.preventDefault();
+
+    setIsSubmitting(true);
+    try {
+      await axios({
+        method: 'POST',
+        url: 'https://formspree.io/f/xwkwogkz',
+        data: new FormData(evt.target),
+      });
+      alert('Thank you, We will be in touch with you soon!');
+      evt.target.reset();
+    } catch (error) {
+      alert(error);
+    }
+
+    setIsSubmitting(false);
+  };
+
     return <div className="service_body" ref={props.scrollTo}> 
         <div className="slogan">
             Contact us now for a free quote!
         </div>
 
         <div className="contact_container">
-            <form className="contact_form">
+            <form onSubmit={handleSubmit} className="contact_form">
                 <div>
                 <input
                     type="text"
                     name="name"
                     placeholder="Full Name"
                     required
+                     disabled={isSubmitting}
                 />
                 <input
                     type="email"
                     name="email"
                     placeholder="Email Address"
                     required
+                     disabled={isSubmitting}
                 />
                 </div>
                 <textarea
                     name="message"
                     placeholder="Hello!!"
                     required
+                     disabled={isSubmitting}
                 />
-                <button type="submit" className="btn btn--orange" style={{ marginTop: '25px' }}>
+                <button type="submit" className="btn btn--orange" style={{ marginTop: '25px' }} disabled={isSubmitting}>
                     Send Message
                 </button>
             </form>
